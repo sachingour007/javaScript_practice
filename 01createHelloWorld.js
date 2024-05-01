6fcce45c397bc45efb576b44cefd75f0fa446241
sachingour <gour.sachin.sg@gmail.com>
2024-05-01 10:45:32 +0530

    updateCode

--- 01createHelloWorld.js
+++ 01createHelloWorld.js
@@ -1,12 +1,11 @@
-function createHelloWorld () {
-    return function (...arg){
-        // console.log(...arg);
-        return console.log("Hello World"); 
-    }
+function createHelloWorld() {
+  return function (...arg) {
+    // console.log(...arg);
+    return console.log("Hello World");
+  };
 }
 
 const ans = createHelloWorld();
 ans(123);
-
 
 //This Function always return Hello World.
