e34a530ac7af953364639d3f5afb12116c075aa5
sachingour <gour.sachin.sg@gmail.com>
2024-12-07 21:33:58 +0530

    color changer

--- ChaiCode/Day1.js
+++ ChaiCode/Day1.js
@@ -1,35 +1,35 @@
-//Task1
-var num = 10;
-console.log(num);
+// //Task1
+// var num = 10;
+// console.log(num);
 
-//Task2
-let chai = "chaiCode";
-console.log(chai);
+// //Task2
+// let chai = "chaiCode";
+// console.log(chai);
 
-chai = "code";
-console.log(chai);
+// chai = "code";
+// console.log(chai);
 
-//Constant Declaration
-//Task3
-const isTrue = true;
-console.log(isTrue);
-
-
-// isTrue = false;
+// //Constant Declaration
+// //Task3
+// const isTrue = true;
 // console.log(isTrue);
 
-//Task4
+// // isTrue = false;
+// // console.log(isTrue);
 
-const arr = [1, 2, 3, 4, 5];
-console.log(typeof arr);
+// //Task4
 
-const obj1 = {
-  name: "sachin",
-  age: 24,
-  welcome: function () {
-    console.log(`Welcome ${this.name}`);
-  },
-};
+// const arr = [1, 2, 3, 4, 5];
+// console.log(typeof arr);
 
-obj1.welcome();
+// const obj1 = {
+//   name: "sachin",
+//   age: 24,
+//   welcome: function () {
+//     console.log(`Welcome ${this.name}`);
+//   },
+// };
+
+// obj1.welcome();
+
 
