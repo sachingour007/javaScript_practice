3a5bfe685d7cdf8e11afc811bbd160b5c26b041c
sachingour <gour.sachin.sg@gmail.com>
2025-01-22 15:50:38 +0530

    curring 2 two type methods

--- Module-Js/KodeKarma Queston/curreingFunction.js
+++ Module-Js/KodeKarma Queston/curreingFunction.js
@@ -8,4 +8,18 @@ const multiply = (a) => {
   };
 };
 
-console.log(multiply(2)(6));
+// console.log(multiply(2)(6));
+
+const multiplyTwo = (a) => {
+  return (...args) => {
+    if (args.length > 0) {
+      console.log(args);
+      const para = args.reduce((acum, num) => acum * num, a);
+      return multiplyTwo(para);
+    } else {
+      return a;
+    }
+  };
+};
+
+console.log(multiplyTwo(2)(3,4)());
